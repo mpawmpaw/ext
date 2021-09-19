ce7bef3979e370cedcaa01e1d2b73d3ea5164367
mpawmpaw <90712820+mpawmpaw@users.noreply.github.com>
2021-09-19 12:40:32 +0200

    Add files via upload

--- mpaw/jskatex/event/main_listener.php
+++ mpaw/jskatex/event/main_listener.php
@@ -1,1 +1,77 @@
+<?php
+/**
+ *
+ * Display KaTeX for phpBB 3.3.2. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2021, Michal Pawlowski
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
 
+namespace mpaw\jskatex\event;
+
+/**
+ * @ignore
+ */
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Display KaTeX for phpBB 3.3.2 Event listener.
+ */
+class main_listener implements EventSubscriberInterface
+{
+	public static function getSubscribedEvents()
+	{
+		return [
+			'core.user_setup'							=> 'load_language_on_setup',
+	        'core.display_forums_modify_template_vars'	=> 'display_forums_modify_template_vars',
+	        'core.modify_submit_post_data'              => 'mod_bbcodes_for_jskatex'
+		];
+	}
+
+	/* @var \phpbb\language\language */
+	protected $language;
+
+	/**
+	 * Constructor
+	 *
+	 * @param \phpbb\language\language	$language	Language object
+	 */
+	public function __construct(\phpbb\language\language $language)
+	{
+		$this->language = $language;
+	}
+
+	/**
+	 * Load common language files during user setup
+	 *
+	 * @param \phpbb\event\data	$event	Event object
+	 */
+	public function load_language_on_setup($event)
+	{
+		$lang_set_ext = $event['lang_set_ext'];
+		$lang_set_ext[] = [
+			'ext_name' => 'mpaw/jskatex',
+			'lang_set' => 'common',
+		];
+		$event['lang_set_ext'] = $lang_set_ext;
+	}
+
+	/**
+	 * A sample PHP event
+	 * Modifies the names of the forums on index
+	 *
+	 * @param \phpbb\event\data	$event	Event object
+	 */
+	public function display_forums_modify_template_vars($event)
+	{
+		$forum_row = $event['forum_row'];
+		$forum_row['FORUM_NAME'] .= $this->language->lang('JSKATEX_EVENT');
+		$event['forum_row'] = $forum_row;
+	}
+	
+	public function mod_bbcodes_for_jskatex($event)
+	{
+	    echo "<script>alert('OK');</script>";
+	}
+}
